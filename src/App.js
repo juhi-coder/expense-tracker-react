3d39b540ccb217576b78bfb7d9e987ac34383f84
juhi-coder <kumarijuhimdb2@gmail.com>
2023-03-24 17:02:55 +0530

    Creating your own first Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses  from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App=()=>
 {
@@ -31,7 +32,7 @@ const App=()=>
 
   return (
   <div>
-  <h2>lets get started</h2>
+  <NewExpense />
   <Expenses item={expenses}/>
   </div>);
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,29 +4,14 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const [newTitle, setNewTitle] = useState("");
-  const changeHandler = (event) => {
-    setNewTitle(event.target.value);
-  };
-
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle(newTitle);
-  };
 
   return (
     <Card className="expense-item">
-      <div className="expense-item__description">
-        <ExpenseDate date={props.date} />
-        <h2>{title}</h2>
-
+    <ExpenseDate date={props.date} />
+      <div className="expense-item__description">    
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <input type="text" value={newTitle} onChange={changeHandler} />
-        <button onClick={clickHandler}>Change Title</button>
       </div>
-
-      {/* <button onClick={clickHandler1}>Change Amount</button> */}
-      {/* <button type="button" onClick={()=> deleteExpenses(props.id)}>Delete Expenses</button> */}
     </Card>
   );
 };
