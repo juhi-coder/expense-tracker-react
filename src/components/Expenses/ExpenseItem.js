dc2e7f28e4210f487332850d12c3d775a9a585d3
juhi-coder <kumarijuhimdb2@gmail.com>
2023-03-24 16:28:04 +0530

    edit+delete title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,40 +1,33 @@
-import React ,{ useState } from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
-import Card from'../UI/Card'
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
-const ExpenseItem=(props)=> {
-  
-  const[expenses,setTitle]= useState(props.expenses)
-  const deleteExpenses=(id)=>{
-    const newList = expenses.filter((props)=>props.id !== id)
-    setTitle(newList);
-  }
+const ExpenseItem = (props) => {
+  const [newTitle, setNewTitle] = useState("");
+  const changeHandler = (event) => {
+    setNewTitle(event.target.value);
+  };
 
-  const[title,newTitle]= useState(props.title);
-  const clickHandler=()=>{
-    newTitle('new Title...');
-  }
-
-  const[title1,newTitle1]= useState(props.amount);
-  const clickHandler1=()=>{
-    newTitle1('100');
-  }
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle(newTitle);
+  };
 
   return (
     <Card className="expense-item">
-      
       <div className="expense-item__description">
-      <ExpenseDate date={props.date}/>
+        <ExpenseDate date={props.date} />
         <h2>{title}</h2>
+
+        <div className="expense-item__price">${props.amount}</div>
+        <input type="text" value={newTitle} onChange={changeHandler} />
         <button onClick={clickHandler}>Change Title</button>
-        
-        <div className="expense-item__price">${title1}</div>
       </div>
-  
-      <button onClick={clickHandler1}>Change Amount</button>
-      <button type="button" onClick={()=> deleteExpenses(props.id)}>Delete Expenses</button>
+
+      {/* <button onClick={clickHandler1}>Change Amount</button> */}
+      {/* <button type="button" onClick={()=> deleteExpenses(props.id)}>Delete Expenses</button> */}
     </Card>
   );
-}
+};
 export default ExpenseItem;
