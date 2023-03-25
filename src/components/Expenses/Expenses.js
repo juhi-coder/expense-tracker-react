51f3dea69f40e5ec87158577f77ffa6da2321375
juhi-coder <kumarijuhimdb2@gmail.com>
2023-03-25 08:37:02 +0530

    PlaceHolder - Adding a filter which doesnt work on the expense tracker

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,21 @@
+import React ,{useState} from "react";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 
 const Expenses=(props)=>
 {
+    const[filteredYear,setFilteredYear]=useState('2020');
+    const filterChangeHandler=selectedYear=>{
+        setFilteredYear(selectedYear);
+    }
 
     return(
     <Card className="expenses">
+    <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
     {
+        
         props.item.map(
             expense=>(
                 <ExpenseItem 
